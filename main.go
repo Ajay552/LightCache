9b111edce2931c9bbb2bf5f0e1011b13e40e12a3
ajay_s <sajayyy01234@gmail.com>
2025-07-04 08:53:01 +0530

    feat: implemented command handlers with ping, get and set commands

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -34,8 +35,32 @@ func main() {
 			return
 		}
 
+		if value.typ != "array" {
+			fmt.Println("Invalid request, expects array")
+			continue
+		}
+
+		if len(value.array) == 0 {
+			fmt.Println("Invalid request, expects array length > 0")
+			continue
+		}
 		fmt.Println(value)
+
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
 		writer := NewWriter(conn)
-		writer.Write(Value{typ: "string", str: "OK"})
+
+		handler, ok := Handlers[command]
+
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+
+		result := handler(args)
+
+		writer.Write(result)
 	}
 }
